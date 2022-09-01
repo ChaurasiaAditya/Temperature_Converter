8d71c3848c91f8e6bd7288d4ee69a119286bafdf
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-01 17:48:48 +0530

    created convertCelsiusToFahrenheit Method

--- src/main/java/com/aditya/TemperatureConverter.java
+++ src/main/java/com/aditya/TemperatureConverter.java
@@ -6,4 +6,19 @@
 package com.aditya;
 
 public class TemperatureConverter {
+
+
+
+    /**
+     * This method takes temperature input in celsius and converts it in fahrenheit.
+     *
+     * @param temperatureInCelsius the temperature in celsius.
+     * @return the converted temperature in fahrenheit.
+     */
+    public double convertCelsiusToFahrenheit(double temperatureInCelsius) {
+        // Write Formula to calculate Fahrenheit from Celsius
+        return (temperatureInCelsius * 1.8) + 32;
+    }
+
 }
+
